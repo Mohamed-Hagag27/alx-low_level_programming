de8e794cb98f3af18891c9fbf8eba999bcfaf623
Mohamed-Hagag27 <Mohamed.abo-elhagag@outlook.com>
2023-03-16 18:18:28 -0700

    5th task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - prints with proper grammar, but the outcome is a piece of art
  * followed by a new line.
- * Return 0
+ * Return: always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
