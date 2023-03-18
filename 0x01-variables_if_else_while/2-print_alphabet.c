2f20286880955ad104af50a9596159d9c39532a6
Mohamed-Hagag27 <Mohamed.abo-elhagag@outlook.com>
2023-03-18 09:35:36 -0700

    lower Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+for (char alphabet = 'a'; alphabet < 123 ; alphabet++)
+putchar(alphabet);
+
+putchar(' ');
+return (0);
+}
