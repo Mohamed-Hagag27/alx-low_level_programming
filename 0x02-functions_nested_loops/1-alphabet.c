447e2c7b8ecdf1ac1906ff4a14a09becda268e42
Mohamed-Hagag27 <Mohamed.abo-elhagag@outlook.com>
2023-03-21 17:10:41 -0700

    print alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase.
+ */
+void print_alphabet(void)
+{
+for (int i = 97 ; i < 123 ; i++)
+_putchar(i);
+_putchar('\n');
+}
