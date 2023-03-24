cf5b9c92bfdbbe1e6ebaa2436beac93b6d02a813
Mohamed-Hagag27 <Mohamed.abo-elhagag@outlook.com>
2023-03-24 15:00:37 -0700

    testing

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,10 +10,10 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
+/*not incrementing the i causes an infinite loop*/
 while (i < 10)
 {
 putchar(i);
-/*not incrementing the i causes an infinite loop*/
 }
 printf("Infinite loop avoided! \\o/\n");
 return (0);
