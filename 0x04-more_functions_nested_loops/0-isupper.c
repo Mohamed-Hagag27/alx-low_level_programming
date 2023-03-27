3b107fcf37d4e71c668c7cf20067da5920f456fc
Mohamed-Hagag27 <Mohamed.abo-elhagag@outlook.com>
2023-03-27 11:16:59 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * _isupper - checks if a char is upper case or lower case.
+ * @c: the variable to check.
+ *
+ * Return: differs.
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+
+return (0);
+}
