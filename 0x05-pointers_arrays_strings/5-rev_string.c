1e2443cc7d4bbe606779ac2ddd6bae0b691cbe7a
Mohamed-Hagag27 <Mohamed.abo-elhagag@outlook.com>
2023-03-27 17:19:42 -0700

    rev as tring

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+/**
+ * rev_string - reverses a string.
+ * @s: pointer to the begining of a string.
+ */
+void rev_string(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+int index = i - 1;
+int c;
+for (c = 0; c < i / 2; c++)
+{
+char ch = s[c];
+s[c] = s[index - c];
+s[index - c] = ch;
+}
+}
