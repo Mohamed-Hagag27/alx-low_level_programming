d13acdf82cfe44d0d7a488301af66bc649643ea0
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-16 17:29:34 -0700

    making a library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of a number
+ * Return: absolute value.
+ * @n: number to print its aboslute value.
+*/
+int _abs(int n)
+{
+if (n < 0)
+return (n * -1);
+else
+return (n);
+}
