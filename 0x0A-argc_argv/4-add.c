cfa486109203d32b4c05be75f37f71053d32acfa
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-17 15:35:47 -0700

    fiinishing the problem

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,41 @@
 #include"main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * main - main function
  * Return: always 0.
  * @argc: number of arguments.
  * @argv: string array containing the arguments.
  */
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char *argv[])
 {
-int i;
+int i, j, length;
+char* ptr;
 int total = 0;
-if (argc <= 2 )
+if (argc < 2)
 printf("%d\n", 0);
 else
 {
 for (i = 1; i < argc ; i++)
 {
-if (atoi(argv[i]) <= 0)
+ptr = argv[i];
+length = strlen(ptr);
+for (j = 0; j < length; j++)
+{
+if (isdigit(*(ptr + j)) == 0)
 {
 printf("Error\n");
 return (1);
 }
-total += atoi(argv[i]);
 }
-printf("%d\n", total);
+total+= atoi(argv[i]);
+}
+printf("%d", total);
 }
 return (0);
 }
