cb796fb32cf71d4c9a42bc2195aec3a3961c9002
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-16 17:12:05 -0700

    adding some files

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _isdigit - checks if the variable is digit or not.
+ * @c: the variable to check.
+ * Return: differs due to the condition result.
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+return (1);
+
+return (0);
+}
