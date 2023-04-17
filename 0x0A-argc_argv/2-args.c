e71b0bd0e53f3648247123b7751097b5fef3b178
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-17 13:38:46 -0700

    printing all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Return: always 0.
+ * @argc: number of arguments.
+ * @argv: string array containing the arguments.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+if (argc == 1)
+printf("%s\n", argv[argc - 1]);
+else
+{
+for (i = 0; i < argc; i++)
+pritnf("%s\n", argv[i]);
+}
+
+return (0);
+}
