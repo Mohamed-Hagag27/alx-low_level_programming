7f7c22bf2b452ea4327e756e7376a53ceec94037
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-17 18:28:17 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A simple struct that contains variables
+ * @name: Holds name of dog in char array
+ * @age: Holds number, including any decimals
+ * @owner: Holds name of owner in char array
+ */
 struct dog
 {
-  char *name;
-  char *owner;
-  float age;
+char *name;
+char *owner;
+float age;
 }
+#endif
