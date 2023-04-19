8121bca26effe8e58a91227b37e43145229a372f
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-19 16:44:46 -0700

    adding return again

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 
 /**
- * print_name - This function prints a name using function pointers
+ * print_name - prints a name using function pointers
  * @name: String to be printed out
  * @f: Function pointer
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL || f != NULL)
+if (name == NULL || f == NULL)
+return;
 f(name);
 }
