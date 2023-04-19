838068579474aa6869495ed964e6ae2ca08c84f9
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-19 08:05:52 -0700

    refining the condition

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,8 @@ char *create_array(unsigned int size, char c)
 char *arr;
 unsigned int i;
 arr = malloc(sizeof(char) * size);
-if (arr == NULL)
-return ('\0');
+if (size == 0)
+return (NULL);
 for (i = 0; i < size; i++)
 {
 arr[i] = c;
