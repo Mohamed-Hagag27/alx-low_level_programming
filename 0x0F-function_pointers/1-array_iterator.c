13bb4752003b0d31226489b7dc40dc6fbb163587
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-19 17:22:28 -0700

    adding stdlib

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-long unsigned int i;
+unsigned int i;
 if (action == NULL || array == NULL || size <= 0)
 return;
 for (i = 0; i < size; i++)
