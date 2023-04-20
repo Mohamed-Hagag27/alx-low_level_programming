0ca957e1cf97caa76068ff623770c89d1eecfb05
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-19 19:03:13 -0700

    calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - main function.
+ * @argc: number of arguments.
+ * @argv: array of the arguments.
+ * Return: always 0.
+ */
+int main(int argc, int *argv[])
+{
+int num1, num2, result;
+int (*op_function)(int, int);
+if ((argv[2] == '/' || argv[2] == '%') && argv[3] == '0')
+{
+printf("Error\n");
+exit(100);
+}
+if (argc < 4 || argv[1] == NULL || argv[3] == NULL)
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op_function = get_op_func(argv[2]);
+if (op_function == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+result = ptr(num1, num2);
+printf("%d\n", result);
+return (0);
+}
