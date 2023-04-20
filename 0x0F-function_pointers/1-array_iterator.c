b86e44bf9677acfc04d2241b75520c71e77b2b24
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-19 17:13:53 -0700

    adding stdef.h

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 long unsigned int i;
-if (action == NULL || array == NULL)
+if (action == NULL || array == NULL || size <= 0)
 return;
 for (i = 0; i < size; i++)
 {
