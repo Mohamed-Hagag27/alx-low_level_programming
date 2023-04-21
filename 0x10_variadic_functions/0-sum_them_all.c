ff83747ae276d207368d857d02df439f40445e73
Mohamed-Hagag27 <Mohamed.AboEl-Hagag@outlook.com>
2023-04-20 17:52:48 -0700

    readme

--- 0x10_variadic_functions/0-sum_them_all.c
+++ 0x10_variadic_functions/0-sum_them_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdio.h>
 /**
  * sum_them_all - take variable number of args and sum them.
  * @n: Number of arguments.
--- 0x10_variadic_functions/README.md
+++ 0x10_variadic_functions/README.md
@@ -1,1 +1,1 @@
-README.md
+Variadic functions
