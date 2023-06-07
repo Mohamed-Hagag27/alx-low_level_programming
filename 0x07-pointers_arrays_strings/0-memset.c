6879bfd8bac9261b32ed0fc354f02a010a31ac3b
Mohamed-Hagag27 <mohamed.aboel-hagag@outlook.com>
2023-06-07 03:34:17 -0700

    return value issue

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,10 +12,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
-for (i = 0; i < n; i++)
+ char *pointer = s;
+ for (i = 0; i < n; i++)
 {
-*s = b;
-s++;
+*pointer = b;
+pointer++;
 }
 return (s);
 }
