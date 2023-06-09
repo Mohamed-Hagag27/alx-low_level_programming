002d5aae3ac847e374561520b00c338f209621c4
Mohamed-Hagag27 <mohamed.aboel-hagag@outlook.com>
2023-06-09 06:00:44 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: list of arguments
+ * Return: 1 or zero
+ */
+
+int main(int argc, char *argv[]) {
+int num1, num2;
+if (argc != 3){
+printf("Error\n");
+return (1);
+}
+
+num1 = atoi(argv[1]);
+
+num2 = atoi(argv[2]);
+
+printf("%d\n", num1 * num2);
+return (0);
+}
