dbb9f24d679bd5ba502e4e94d18d4fcf1ee668b0
Mohamed-Hagag27 <mohamed.aboel-hagag@outlook.com>
2023-06-16 09:50:50 -0700

    allocating 2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,14 +16,28 @@ int **arr = NULL;
 if (width <= 0 || height <= 0)
 return (NULL);
 
-arr = malloc( ((height * width) + 1) * sizeof(int));
+arr = malloc(height * sizeof(int*));
 if (arr == NULL)
 return (NULL);
+
+for(i = 0 ; i < height; i++)
+{
+arr[i] = malloc(width * sizeof(int));
+if(arr[i] == NULL)
+{
+for(--i ; i >= 0;)
+{
+free(arr[i]);
+}
+return (NULL);
+}
+}
 
 for (i = 0; i < height; i++)
 {
 for (j = 0; j < width; j++)
 arr[i][j] = 0;
+
 }
 return (arr);
 }
