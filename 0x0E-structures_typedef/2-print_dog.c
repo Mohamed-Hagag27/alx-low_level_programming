4442e848b0b8940db26064a71cec6bbc0234ff80
Mohamed-Hagag27 <mohamed.aboel-hagag@outlook.com>
2023-06-19 16:21:37 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,35 +3,18 @@
 #include <stdlib.h>
 
 /**
- * _isdigit - checks if the variable is digit or not.
- * @c: the variable to check.
- * Return: differs due to the condition result.
- */
-int _isdigit(int c)
-{
-if (c >= 48 && c <= 57)
-return (1);
-
-return (0);
-}
-
-/**
  * print_dog - print dog struct members
  * @d: pointer to the dog struct
  */
 
 void print_dog(struct dog *d)
 {
-int n = (int) d->age %10;
 if (d == NULL)
 return;
 if (d->name == NULL)
 printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
-if (!_isdigit(n))
-printf("age: (nil)\n");
-else
 printf("Age: %.6f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
