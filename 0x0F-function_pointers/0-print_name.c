c85959c35673822a7caa078114ad632d944441e6
Mohamed-Hagag27 <mohamed.aboel-hagag@outlook.com>
2023-06-25 03:44:12 -0700

    printing name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - makes the callback function print the string given
+ * @name: the string to be printed
+ * @f: pointer to the callback function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
